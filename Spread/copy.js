78af38477eb24a2e93bb5bfdec8aac41b16edfa4
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-06 07:22:12 +0530

    update

---
+++ Spread/copy.js
@@ -0,0 +1,12 @@
+const arr1 = [1, 2, 3];
+
+const arr2 = [...arr1];
+
+arr1[0] = 18;
+console.log(arr1, arr2);
+
+const obj1 = { one: 10, two: 20, three: 30 };
+const obj2 = { ...obj1 };
+obj1["one"] = 15;
+
+console.log(obj1, obj2);
