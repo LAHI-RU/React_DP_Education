bd55011dee1d194e52821bed891fbf5f8a6dee57
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-07 07:40:49 +0530

    update

--- 04-Rest/Rest/array_destructuring.js
+++ 04-Rest/Rest/array_destructuring.js
@@ -1,2 +1,16 @@
 const arr1 = [1, 2, 3, 4, 5];
+
+// let a = arr1[0];
+// let b = arr1[1];
+
 const [first, second, ...rest_arguments] = arr1;
+console.log(first);
+console.log(second);
+console.log(rest_arguments);
+
+const obj1 = { one: 10, two: 20, three: 30, four: 40, five: 50 };
+const { one, two, ...rest_obj } = obj1;
+
+console.log(one);
+console.log(two);
+console.log(rest_obj);
