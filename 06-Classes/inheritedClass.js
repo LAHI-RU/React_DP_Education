4f278d09313dd203ee7c1b238ee9be756cbe7bc9
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-11 14:13:30 +0530

    update

---
+++ 06-Classes/inheritedClass.js
@@ -0,0 +1,28 @@
+class person {
+  constructor(_name, _age) {
+    this.name = _name;
+    this.age = _age;
+    console.log("A new Person Created");
+  }
+}
+
+class programmer extends person {
+  constructor(_name, _age, _language) {
+    super(_name, _age);
+    this.name = _name;
+    this.age = _age;
+    this._language = _language;
+  }
+
+  code() {
+    return `${this.name} is a programmer &
+        knows ${this._language}`;
+  }
+}
+
+const john = new person("John", "32");
+
+const jane = new programmer("Jane", "28", "JavaScript");
+console.log(jane.code());
+
+console.log(john.code());
