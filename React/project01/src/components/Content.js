12f5aaf4e5493496c2a05adaa35332316cec4bf5
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-16 23:02:48 +0530

    update

---
+++ React/project01/src/components/Content.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./content.css";
+
+export default function Content() {
+  return (
+    <div className="paragraph">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        dolore a accusamus unde optio sint debitis provident illum maiores,
+        velit labore ex consequatur aperiam quibusdam eos fugit veniam nisi
+        veritatis! Soluta labore quaerat impedit sunt ab ducimus dicta iste?
+        Quam voluptatum provident dignissimos deserunt, asperiores, eveniet
+        architecto eos culpa quasi nesciunt et nihil adipisci odio omnis. Enim,
+        laborum! Tempora, iste odit! Modi quo corporis voluptas ut rem iure
+        doloremque adipisci sunt quibusdam pariatur consequuntur ipsa, fugiat
+        culpa necessitatibus, commodi aut ad explicabo? Autem, laboriosam ullam
+        cumque sunt explicabo, reprehenderit vitae ab odit corporis repudiandae
+        totam consectetur quaerat esse asperiores sit.
+      </p>
+    </div>
+  );
+}
