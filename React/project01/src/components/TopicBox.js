2d4a18b7ab301f2e44154f22f467dd300b57f655
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-16 23:16:27 +0530

    update

--- React/project01/src/components/TopicBox.js
+++ React/project01/src/components/TopicBox.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./topicbox.css";
 
 export default function TopicBox() {
-  const topic = "My favourite food is Kottu";
-  const item = " and rice";
+  //   const topic = "My favourite food is Kottu";
+  //   const item = " and hoppers";
+
+  //   const a = 10;
+  //   const b = 20;
+  //   const c = a + b;
+
+  const foodprices = { kottu: "250", rice: "350" };
+
   return (
     <div>
       <div className="topicBox">
-        <span className="text">
-          {" "}
-          {topic} {item}{" "}
-        </span>
+        <span className="text">{foodprices}</span>
       </div>
     </div>
   );
