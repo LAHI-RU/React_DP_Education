6b5cdc0215ac5b1e334ea503796bdb81d6a890ef
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-17 19:24:44 +0530

    props

--- React/project01/src/components/TopicBox.js
+++ React/project01/src/components/TopicBox.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./topicbox.css";
 
-export default function TopicBox() {
+export default function TopicBox(props) {
   return (
     <div>
       <div className="topicBox">
-        <span className="text">Kottu</span>
+        <span className="text">My favourite food is {props.food}</span>
       </div>
     </div>
   );
