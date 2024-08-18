97d6101f88bcdbf180e1df5ff00780217431868e
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-18 07:22:29 +0530

    Update Home.js

--- React/project01/src/Pages/Home.js
+++ React/project01/src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TopicBox from "../components/TopicBox";
 import Content from "../components/Content";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <h1> Homepage </h1>
+      <Link to="/About"> Go to about page </Link>
     </div>
   );
 }
