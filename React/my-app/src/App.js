b98105a9a155bf61f894ec514e441774f20d568d
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-23 09:05:28 +0530

    update

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import Accordion from "react-bootstrap/Accordion";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
   return (
     <div>
+      <>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
       <h1> React Bootstrap </h1>
       <div className="accordin">
         <Accordion defaultActiveKey="0">
