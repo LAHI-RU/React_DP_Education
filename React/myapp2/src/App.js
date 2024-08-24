b65ef58b4447de1e66b75acc9cd7a610eb6668ba
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-08-24 13:35:13 +0530

    update

--- React/myapp2/src/App.js
+++ React/myapp2/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Profile from "./Profile";
+import About from "./About";
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/profile" element={<profile />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/about" element={<About />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
